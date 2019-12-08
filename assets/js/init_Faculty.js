8f97262683d4a0362ebf835a679c7276545c5d97
Nakzz <naqibprio10@gmail.com>
2019-12-08 00:25:28 -0600

    Cleaned up javascript errors

--- assets/js/init_Faculty.js
+++ assets/js/init_Faculty.js
@@ -22,17 +22,7 @@
 
 "use strict";
 
-/*************************************/
-/* Preloader */
-/**************************************/
-jQuery(window).load(function() {
-        // will first fade out the loading animation
-	jQuery(".status").fadeOut();
-        // will fade out the whole DIV that covers the website.
-	jQuery(".preloader").delay(100).fadeOut("slow");
-	jQuery("body").css('overflow-y','visible');
 
-});
 
 /*************************************/
 /* Ready Function */
