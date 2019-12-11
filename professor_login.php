8eed1cf864b29b96e944c4d2ad90ecb3f650d6e1
Diana Pena-Moreno <dianapenamoreno@MacBook-Pro-2.local>
2019-12-10 23:25:10 -0600

    added styling to prof login

--- professor_login.php
+++ professor_login.php
@@ -7,59 +7,47 @@
         <?php 
         $title = "Student_Update";
 
-$record_id = $_POST['record_id'];
-$actionCode = $_POST['actionCode'];
-$keyword;
 
-if($actionCode==1){
-    $keyword="ADD";
-}elseif($actionCode==2){
-    $keyword="DELETE";
-}
 
-include("./includes/connect.inc.php");
+
 include_once("./includes/head.php");
-include('./includes/ChromePhp.php');
 
-if(isset($_POST['submit_button'])){
 
-    ChromePhp::log("Post submit");
-    
-    $student_4digit = $_POST['student_4digit'];
-    $student_lname = $_POST['student_lname'];
-    $sentence;
-    
-    if ($actionCode==1){
-        $sentence = "UPDATE RECORD SET student_4_digit=\"$student_4digit\", student_lname=\"$student_lname\" WHERE record_id=\"$record_id\"";
-    }elseif($actionCode==2){
-        $sentence = "UPDATE RECORD SET student_4_digit=NULL, student_lname=NULL WHERE (record_id=\"$record_id\") AND (student_4_digit=\"$student_4digit\") AND (student_lname=\"$student_lname\")";
+    include("./includes/connect.inc.php");
+    function set_session($id,$result_set){
+        session_start();
+        $sess_id = session_id();
+        $_SESSION['loginID']=$id;
+        $_SESSION['miscellaneous']="fuckyou";
+        $row = mysqli_fetch_assoc($result_set);
+        $_SESSION['prof_name']=$row["prof_name"];
     }
 
-    $result = mysqli_query($conn, $sentence);
-    if (!$result){
-        die("cannot processed select query");
-    }
-    elseif(mysqli_affected_rows($conn)<=0){
-
-        ChromePhp::log("Sql query was made succ");
-
-        if($actionCode==1){
-            echo "<script type='text/javascript'>alert('Unknown failure');</script>";
-            echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=scheduler.php\">";
+    if($_POST['submit_button']){
+        $usrname=$_POST['username'];
+        $passwd=$_POST['password'];
+        $query_sentence="SELECT * FROM PROFESSOR WHERE (fac_id=\"$usrname\") AND (passwd=\"$passwd\")";
+        $auth_result = mysqli_query($conn,$query_sentence);
+        if(!$auth_result){
+            echo "<script type='text/javascript'>alert('connection failed!');</script>";
+            echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=professor_login.php\">";
+        }else{
+            $num = mysqli_num_rows($auth_result);
+            if($num==0){
+                echo "<script type='text/javascript'>alert('incorrect username or password!');</script>";
+                echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=professor_login.php\">";
+            }elseif($num==1){
+                echo "<script type='text/javascript'>alert('Logged in!');</script>";
+                echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=professor_cPanel.php\">";
+                set_session($usrname,$auth_result);
+                exit;
+            }else{
+                echo "<script type='text/javascript'>alert('Database Error!');</script>";
+                echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=professor_login.php\">";
+            }
         }
-        elseif($actionCode==2){
-            echo "<script type='text/javascript'>alert('4 digit id or lastname mismatch!');</script>";
-            echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=scheduler.php\">";
-        }
-    }else{
-        echo "<script type='text/javascript'>alert('Successfully Updated!');</script>";
-        echo "<META HTTP-EQUIV=\"refresh\" content=\"0; URL=scheduler.php\">";
     }
-}
-
-ChromePhp::log("Past _POST");
-ChromePhp::log($_POST);
-
+    mysqli_close($conn);
 
 
         ?>      
