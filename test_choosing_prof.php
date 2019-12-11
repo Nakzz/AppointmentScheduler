9ad953f73362741697550f2ab07c73da56698c18
Diana Pena-Moreno <dianapenamoreno@MacBook-Pro-2.local>
2019-12-10 19:16:48 -0600

    deleted test_choosing_prof and added new

--- test_choosing_prof.php
+++ test_choosing_prof.php
@@ -45,30 +45,6 @@
 						What will you ask?</h3>
 						
 <br> </br>
-
-<center><table border="3">
-	<tr>
-		<td>
-    		<input type="hidden" name="fac_id" value=testAdmin>
-        	<img src="assets/img/krista.jpg" width="115" height="130" alt="" /> 
-        	<center><a href="javascript:;" onclick="parentNode.submit();"><font color=white>Krista-Lee Malone <br></br></font></a> </center>
-    	</td>
-		
- 		<td>
- 			<p> <br> </br> <br></p>
-
- 		</td>
- 		<td>
-        	<form action="faculty.php" method="post">
-        	<img src="assets/img/meghan.jpg" width="105" height="130" alt="" /> 
-    		<a href="javascript:;" onclick="parentNode.submit();"><font color=white><center> Meghan Mitchell </center><br></br></font></a>
- 		</td>
- 	
-    </tr>
-</table>
-</center>
-
-
 		<form action="faculty_meg.php" method="post">
         <a href="javascript:;" onclick="parentNode.submit();"><font color=white> Meghan Mitchell</font></a> <br></br>
         </form>
@@ -77,7 +53,6 @@
         <a href="javascript:;" onclick="parentNode.submit();"><font color=white>Krista-Lee Malone</font></a>
         <input type="hidden" name="fac_id" value=testAdmin>
          
-
   	   </form>
   	   
 					</div>
